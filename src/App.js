f369a7cf9dce898193526e0769e0cb76c60a2663
Sungwon Han <holyhansss@gmail.com>
2021-12-28 18:54:41 +0900

    refactor little bit

--- src/App.js
+++ src/App.js
@@ -3,27 +3,26 @@ import Header from './componment/header/header.js';
 import Contents from './componment/contents/contents.js';
 import Footer from './componment/footer/footer.js';
 import {firebase} from "./firebase";
-import { addDoc, collection } from "firebase/firestore";
-import { getFirestore } from "firebase/firestore"
+import { getFirestore, addDoc, collection } from "firebase/firestore";
 
 function App() {
   const [year, setYear] = useState(2021);
-  const aa = async () => {
-    const db = getFirestore();
-    const docRef = await addDoc(collection(db, "users"), {
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
-    });
-    //firestore.collection("example").add({text: "꿀잠 자기", completed: true});
-  }
+  // const aa = async () => {
+  //   const db = getFirestore();
+  //   const docRef = await addDoc(collection(db, "users"), {
+  //     first: "Ada",
+  //     last: "Lovelace",
+  //     born: 1815
+  //   });
+  //   //firestore.collection("example").add({text: "꿀잠 자기", completed: true});
+  // }
 
   return (
     <div>
       <Header></Header>
       <Contents year={year}></Contents>
       <Footer></Footer>
-      <button onClick={aa}>aa</button>
+      {/* <button onClick={aa}>aa</button> */}
     </div>
 
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,9 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // import { getAnalytics } from "firebase/analytics";
-// import firebase from 'firebase/compat/app';
-// import 'firebase/compat/analytics'
-// import 'firebase/compat/auth';
-// import 'firebase/compat/firestore';
-// import 'firebase/compat/firestore';
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/analytics'
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -25,23 +13,5 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-// Initialize Firebase
+
 export default firebase.initializeApp(firebaseConfig);
-// const firebaseInstance = firebase.initializeApp(firebaseConfig);
-
-//export const auth = firebase.auth();
-//export const firestore = firebase.firestore();
-
-//export default firebase;
-//export const firebase = firebaseInstance;
-// export const authService = firebaseInstance.authService();
-// export const firestore = firebaseInstance.firestore;
-// export const storageService = firebaseInstance.storage();
-
-
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// const firestore = firebase.firestore;
-// export default {firestore};
