48c427357a5ddc4672fdaec3a71f6a7d67dfa042
Sungwon Han <holyhansss@gmail.com>
2022-01-20 10:45:18 +0900

    erase unused propertys

--- src/pages/proposalPage.js
+++ src/pages/proposalPage.js
@@ -1,8 +1,6 @@
-import {React, useState, useEffect} from 'react';
+import { React } from 'react';
 import Header from '../componment/header/header.js';
 import Footer from '../componment/footer/footer.js';
-import { Typography, Box, Button, TextField, Select, MenuItem, FormControl, InputLabel, makeStyles } from '@material-ui/core';
-import styled from 'styled-components';
 import ProposalForm from '../componment/proposalForm/proposalForm.js';
 
 
