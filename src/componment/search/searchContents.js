4e3e7bc0b6a3e98cf8328f5d74ac7a0088edd092
Sungwon Han <holyhansss@gmail.com>
2022-01-27 23:06:47 +0900

    search contents

---
+++ src/componment/search/searchContents.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledDiv = styled.div`
+height: 500px;
+`;
+
+const SearchContents = () => {
+
+    return (
+        <StyledDiv/>
+    )
+};
+
+export default SearchContents;
--- src/pages/searchPage.js
+++ src/pages/searchPage.js
@@ -1,7 +1,7 @@
 import { React } from 'react';
 import Header from '../componment/header/header.js';
 import Footer from '../componment/footer/footer.js';
-
+import SearchContents from '../componment/search/searchContents.js';
 
 function SearchPage() {
   
@@ -9,7 +9,7 @@ function SearchPage() {
   return (
     <div>
       <Header/>
-
+      <SearchContents/>
       <Footer/>
     </div>
   );
