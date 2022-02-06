28dc2108ab4bdd1713176d897d2a293f43b0770a
Sungwon Han <holyhansss@gmail.com>
2022-02-06 17:15:42 +0900

    removed unused import

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -2,7 +2,6 @@ import {React, useState, useEffect} from 'react';
 import Header from '../componment/header/header.js';
 import Contents from '../componment/contents/contents.js';
 import Footer from '../componment/footer/footer.js';
-import { SEMESTERS } from '../commons/constants';
 
 function MainPage() {
   
