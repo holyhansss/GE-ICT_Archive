6e3b53d8e0035c3258f12ffb006c1c6d77690576
Sungwon Han <holyhansss@gmail.com>
2022-04-21 23:43:39 +0900

    add approvalPageContent updated to V2

--- src/componment/contents/approvalPageContent.js
+++ src/componment/contents/approvalPageContent.js
@@ -2,16 +2,19 @@ import  { React, useState, useEffect } from 'react';
 
 import { firebase } from "../../firebase";
 import { getFirestore, collection, getDocs, where, query, limit, startAfter, startAt, orderBy, doc, updateDoc, addDoc } from "firebase/firestore";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import oc from 'open-color';
-import { Card, CardHeader, CardContent, TextField, makeStyles, Select, MenuItem ,Button} from "@material-ui/core";
+// import oc from 'open-color';
+//import { Card, CardHeader, CardContent, TextField, makeStyles, Select, MenuItem ,Button} from "@material-ui/core";
+import { Button, Container, Card, Row, Col} from "react-bootstrap";
 
 const ApprovalPageContent = () => {
 
     const db = getFirestore();
     const [content, setContent] = useState([]);
     const [loading, setLoading] = useState(true);
+
+    const navigate = useNavigate();
 
     useEffect(()=> {
         const fetchContent = async () => {
@@ -26,7 +29,7 @@ const ApprovalPageContent = () => {
                 })
                 
             })
-            setLoading(false)
+            if(content.length > 0) setLoading(false);
         }
         fetchContent();        
     },[])
@@ -34,74 +37,124 @@ const ApprovalPageContent = () => {
     useEffect(()=> {
     },[content])
 
-    const updatee = async () => {
-        let q = await query(collection(db, 'CourseProjects'))
-        getDocs(q).then(async (snapshot) => {
-            snapshot.forEach(async (doc)=> {
-                console.log(doc.data())
-                await addDoc(collection(db, "CourseProjects"), doc.data());
+    // const updatee = async () => {
+    //     let q = await query(collection(db, 'CourseProjects'))
+    //     getDocs(q).then(async (snapshot) => {
+    //         snapshot.forEach(async (doc)=> {
+    //             console.log(doc.data())
+    //             await addDoc(collection(db, "CourseProjects"), doc.data());
                 
-            })
-            //await addDoc(collection(db, "CourseProjects"), snapshot.data());
-        })
+    //         })
+    //         //await addDoc(collection(db, "CourseProjects"), snapshot.data());
+    //     })
 
-    }
+    // }
 
     const handleApproval = async (e, document, index) => {
         await updateDoc(doc(db, "CourseProjects", document.id),{
             "approved": true,
         }).then((e) => {
-            alert("approved!")
+            alert("approved!" + document.teamName);
             var filtered = content.filter(function(value, index, arr){ 
                 return document !== value;
             });
-            setContent(filtered)
-            //console.log(filtered)
+            setContent(filtered);
 
         });
     }
-
-    const list = content.map((doc, index) => (
-        loading === false ?
-        <Card key={index} className="">
-            <Link key={index}
-            to={`/detailpages/${index}`}
-            state={{
+    const handleCardClick = (index, doc) => {
+        navigate(`/detailpages/${index}`, {
+            state: {
                 id: index,
                 course: doc.course,
-                contentInfo: doc,
-            }}
-            className=""
-            style={{ textDecoration: 'none' }}
-            >
-                <CardContent>
-                    <img className="" src={doc.image_url}></img>
-                </CardContent>
-                <CardHeader title={doc.teamName} className=""></CardHeader>
+                contentInfo: doc
+            }
+        });
+    }
+
+    // const list = content.map((doc, index) => (
+    //     loading === false ?
+    //     <Card key={index} className="">
+    //         <Link key={index}
+    //         to={`/detailpages/${index}`}
+    //         state={{
+    //             id: index,
+    //             course: doc.course,
+    //             contentInfo: doc,
+    //         }}
+    //         className=""
+    //         style={{ textDecoration: 'none' }}
+    //         >
+    //             <CardContent>
+    //                 <img className="" src={doc.image_url}></img>
+    //             </CardContent>
+    //             <CardHeader title={doc.teamName} className=""></CardHeader>
                 
-                <CardContent>
-                    {
-                    doc.hashTag !== undefined ?
-                    doc.hashTag.map((tag, index)=> 
-                        <div className="" key={index}>{tag}</div>
-                    )
-                    : <div></div>
-                    }       
-                </CardContent>
-            </Link>
-            <div className="" onClick={(e) => {handleApproval(e, doc, index)}}>Approve</div>
-        </Card>
-        : <Card key={index}></Card>
-    ));
+    //             <CardContent>
+    //                 {
+    //                 doc.hashTag !== undefined ?
+    //                 doc.hashTag.map((tag, index)=> 
+    //                     <div className="" key={index}>{tag}</div>
+    //                 )
+    //                 : <div></div>
+    //                 }       
+    //             </CardContent>
+    //         </Link>
+    //         <div className="" onClick={(e) => {handleApproval(e, doc, index)}}>Approve</div>
+    //     </Card>
+    //     : <Card key={index}></Card>
+    // ));
 
     return (
         <div className="">{
-            list.length !== 0
-            ? list
-            : <div className="">Nothing To Approved</div>
+            loading === false
+            ? <Row xs={1} md={3} className="mx-5 g-4 my-3">
+            {
+                content.map((doc, index) => (
+                    <Col
+                        key={index}
+                        className='px-5'>
+                        <Card
+                            onClick={() => {
+                                handleCardClick(index, doc)
+                            }}>
+                            <Card.Img variant="top" src={doc.image_url}/>
+                            <Card.Body>
+                                    <Card.Title>{doc.teamName}</Card.Title>
+                                {
+                                    doc.hashTag !== undefined
+                                        ? doc
+                                            .hashTag
+                                            .map(
+                                                (tag, index) => <div
+                                                    key={index}
+                                                    style={{
+                                                        display: 'inline-block',
+                                                        padding: "2px 5px 2px 5px",
+                                                        fontSize: "13px",
+                                                        fontWeight: "bold"
+                                                    }}>#{tag}</div>
+                                            )
+                                        : <></>
+                                }
+                                <br></br>
+
+                            </Card.Body>
+                        </Card>
+                        <Button onClick={(e) => {handleApproval(e, doc, index)}} className="mt-2"> Approve </Button>
+                    </Col>
+                ))
+            }
+            </Row>
+            : <Container className="" style={{
+                height: '80vh',
+            }}>
+                  <Row className='justify-content-md-center mt-5'>
+                    <Col md="auto"><h1><strong>Nothing To Approved</strong></h1></Col>
+                </Row>
+            </Container>
             }</div>
 
-            //<div onClick={updatee}>update</div>
     );
 }
 
